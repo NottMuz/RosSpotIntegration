8d8bcc3e7d2857dfe60d4aa2c4c733f383c25264
NottMuz <raymanm786@gmail.com>
2024-08-15 17:35:02 -0400

    spot api added

--- publisher_KeyboardInputs.py
+++ publisher_KeyboardInputs.py
@@ -2,7 +2,60 @@
 
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import qos_profile_default
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+import sys, select, termios, tty
+
+
+settings = termios.tcgetattr(sys.stdin)
+
+msg = """
+Reading from the keyboard  and Publishing to Twist!
+---------------------------
+Moving around:
+   u    i    o
+   j    k    l
+   m    ,    .
+
+For Holonomic mode (strafing), hold down the shift key:
+---------------------------
+   U    I    O
+   J    K    L
+   M    <    >
+
+t : up (+z)
+b : down (-z)
+
+anything else : stop
+
+q/z : increase/decrease max speeds by 10%
+w/x : increase/decrease only linear speed by 10%
+e/c : increase/decrease only angular speed by 10%
+
+CTRL-C to quit
+"""
+
+moveBindings = {
+		'i':(1,0,0,0),
+		'o':(1,0,0,-1),
+		'j':(0,0,0,1),
+		'l':(0,0,0,-1),
+		'u':(1,0,0,1),
+		',':(-1,0,0,0),
+		'.':(-1,0,0,1),
+		'm':(-1,0,0,-1),
+		'O':(1,-1,0,0),
+		'I':(1,0,0,0),
+		'J':(0,1,0,0),
+		'L':(0,-1,0,0),
+		'U':(1,1,0,0),
+		'<':(-1,0,0,0),
+		'>':(-1,-1,0,0),
+		'M':(-1,1,0,0),
+		't':(0,0,1,0),
+		'b':(0,0,-1,0),
+	       }
 
 class KeyPublisher(Node):
     def __init__(self):
@@ -10,7 +63,7 @@ class KeyPublisher(Node):
         self.publisher_ = self.create_publisher(String, 'spot_keypress', 10)
         self.get_logger().info('Key Publisher Node has been started.')
 
-        # Simulating keypresses
+        
         self.timer = self.create_timer(1.0, self.publish_key)
 
     def publish_key(self):
@@ -18,6 +71,13 @@ class KeyPublisher(Node):
         msg.data = 'a'  # Simulating 'a' key press for stop command
         self.publisher_.publish(msg)
         self.get_logger().info(f'Published: {msg.data}')
+
+    def getKey():
+        tty.setraw(sys.stdin.fileno())
+        select.select([sys.stdin], [], [], 0)
+        key = sys.stdin.read(1)
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+        return key
 
 def main(args=None):
     rclpy.init(args=args)
--- subscriber_SpotControl.py
+++ subscriber_SpotControl.py
@@ -1,15 +1,192 @@
-import rclpy
-from rclpy.node import Node
-from std_msgs.msg import String
+''' SPOT API ("Back-End) '''
+
+import curses
+import io
+import logging
+import math
+import os
+import signal
+import sys
+import threading
+import time
+from collections import OrderedDict
+
+from PIL import Image, ImageEnhance
+
+import bosdyn.api.basic_command_pb2 as basic_command_pb2
+import bosdyn.api.power_pb2 as PowerServiceProto
+# import bosdyn.api.robot_command_pb2 as robot_command_pb2
+import bosdyn.api.robot_state_pb2 as robot_state_proto
+import bosdyn.api.spot.robot_command_pb2 as spot_command_pb2
+import bosdyn.client.util
+from bosdyn.api import geometry_pb2
+from bosdyn.client import ResponseError, RpcError, create_standard_sdk
+from bosdyn.client.async_tasks import AsyncGRPCTask, AsyncPeriodicQuery, AsyncTasks
+from bosdyn.client.estop import EstopClient, EstopEndpoint, EstopKeepAlive
+from bosdyn.client.frame_helpers import ODOM_FRAME_NAME
+from bosdyn.client.image import ImageClient
+from bosdyn.client.lease import Error as LeaseBaseError
+from bosdyn.client.lease import LeaseClient, LeaseKeepAlive
+from bosdyn.client.power import PowerClient
+from bosdyn.client.robot_command import RobotCommandBuilder, RobotCommandClient
+from bosdyn.client.robot_state import RobotStateClient
+from bosdyn.client.time_sync import TimeSyncError
+from bosdyn.util import duration_str, format_metric, secs_to_hms
+
+LOGGER = logging.getLogger()
+
+VELOCITY_BASE_SPEED = 0.5  # m/s
+VELOCITY_BASE_ANGULAR = 0.8  # rad/sec
+VELOCITY_CMD_DURATION = 0.6  # seconds
+COMMAND_INPUT_RATE = 0.1
+
+def _grpc_or_log(desc, thunk):
+    try:
+        return thunk()
+    except (ResponseError, RpcError) as err:
+        LOGGER.error('Failed %s: %s', desc, err)
+
+
+
+class ExitCheck(object):
+    """A class to help exiting a loop, also capturing SIGTERM to exit the loop."""
+
+    def __init__(self):
+        self._kill_now = False
+        signal.signal(signal.SIGTERM, self._sigterm_handler)
+        signal.signal(signal.SIGINT, self._sigterm_handler)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, _type, _value, _traceback):
+        return False
+
+    def _sigterm_handler(self, _signum, _frame):
+        self._kill_now = True
+
+    def request_exit(self):
+        """Manually trigger an exit (rather than sigterm/sigint)."""
+        self._kill_now = True
+
+    @property
+    def kill_now(self):
+        """Return the status of the exit checker indicating if it should exit."""
+        return self._kill_now
+
+
+class CursesHandler(logging.Handler):
+    """logging handler which puts messages into the curses interface"""
+
+    def __init__(self, wasd_interface):
+        super(CursesHandler, self).__init__()
+        self._wasd_interface = wasd_interface
+
+    def emit(self, record):
+        msg = record.getMessage()
+        msg = msg.replace('\n', ' ').replace('\r', '')
+        self._wasd_interface.add_message(f'{record.levelname:s} {msg:s}')
+
+
+class AsyncRobotState(AsyncPeriodicQuery):
+    """Grab robot state."""
+
+    def __init__(self, robot_state_client):
+        super(AsyncRobotState, self).__init__('robot_state', robot_state_client, LOGGER,
+                                              period_sec=0.2)
+
+    def _start_query(self):
+        return self._client.get_robot_state_async()
+
+class SpotControl(object):
+    """A curses interface for driving the robot."""
+
+    def __init__(self, robot):
+        self._robot = robot
+        # Create clients -- do not use the for communication yet.
+        self._lease_client = robot.ensure_client(LeaseClient.default_service_name)
+        try:
+            self._estop_client = self._robot.ensure_client(EstopClient.default_service_name)
+            self._estop_endpoint = EstopEndpoint(self._estop_client, 'GNClient', 9.0)
+        except:
+            # Not the estop.
+            self._estop_client = None
+            self._estop_endpoint = None
+        self._power_client = robot.ensure_client(PowerClient.default_service_name)
+        self._robot_state_client = robot.ensure_client(RobotStateClient.default_service_name)
+        self._robot_command_client = robot.ensure_client(RobotCommandClient.default_service_name)
+        self._robot_state_task = AsyncRobotState(self._robot_state_client)
+        
+        self._async_tasks = AsyncTasks([self._robot_state_task])
+        self._lock = threading.Lock()
+
+        self._locked_messages = ['', '', '']  # string: displayed message for user
+        self._estop_keepalive = None
+        self._exit_check = None
+
+        # Stuff that is set in start()
+        self._robot_id = None
+        self._lease_keepalive = None
+
+    def start(self):
+        """Begin communication with the robot."""
+        # Construct our lease keep-alive object, which begins RetainLease calls in a thread.
+        self._lease_keepalive = LeaseKeepAlive(self._lease_client, must_acquire=True,
+                                               return_at_exit=True)
+
+        self._robot_id = self._robot.get_id()
+        if self._estop_endpoint is not None:
+            self._estop_endpoint.force_simple_setup(
+            )  # Set this endpoint as the robot's sole estop.
+
+    def shutdown(self):
+        """Release control of robot as gracefully as possible."""
+        LOGGER.info('Shutting down WasdInterface.')
+        if self._estop_keepalive:
+            # This stops the check-in thread but does not stop the robot.
+            self._estop_keepalive.shutdown()
+        if self._lease_keepalive:
+            self._lease_keepalive.shutdown()
+
+    def flush_and_estop_buffer(self, stdscr):
+        """Manually flush the curses input buffer but trigger any estop requests (space)"""
+        key = ''
+        while key != -1:
+            key = stdscr.getch()
+            if key == ord(' '):
+                self._toggle_estop()
+
+    def add_message(self, msg_text):
+        """Display the given message string to the user in the curses interface."""
+        with self._lock:
+            self._locked_messages = [msg_text] + self._locked_messages[:-1]
+
+    def message(self, idx):
+        """Grab one of the 3 last messages added."""
+        with self._lock:
+            return self._locked_messages[idx]
+
+    @property
+    def robot_state(self):
+        """Get latest robot state proto."""
+        return self._robot_state_task.proto
+
+
+
+
+
+
+
+
+
+
+#########################################################################################################################################################################################################
+'''ROS SUBSCRIPTION NODE ("Front-End")'''
 
 class KeySubscriber(Node):
     def __init__(self):
         super().__init__('key_subscriber')
-        self.subscription = self.create_subscription(
-            String,
-            'spot_keypress',
-            self.listener_callback,
-            10)
+        self.subscription = self.create_subscription(String,'spot_keypress',self.listener_callback,10)
         self.get_logger().info('Key Subscriber Node has been started.')
 
     def listener_callback(self, msg):
