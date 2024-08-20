55e8cffe3445598ffc82f941be16a216b32824eb
Muzz04 <raymanm786@gmail.com>
2024-08-20 12:41:38 -0400

    Final revision of the publisher code before adopting "Teleop_Twist_Keyboard" publisher from ROS2. There was no need to reinvent the wheel with my code and, although not very small, big changes are not needed to convert it to my use case.

--- publisher_KeyboardInputs.py
+++ publisher_KeyboardInputs.py
@@ -4,57 +4,37 @@ import rclpy
 from rclpy.node import Node
 from rclpy.qos import qos_profile_default
 from std_msgs.msg import String
-from geometry_msgs.msg import Twist
+# from geometry_msgs.msg import Twist
 import sys, select, termios, tty
 
 
 settings = termios.tcgetattr(sys.stdin)
 
 msg = """
-Reading from the keyboard  and Publishing to Twist!
----------------------------
-Moving around:
-   u    i    o
-   j    k    l
-   m    ,    .
 
-For Holonomic mode (strafing), hold down the shift key:
----------------------------
-   U    I    O
-   J    K    L
-   M    <    >
+Commands: 
+[TAB]: Shut down and quit
+[SPACE]: Estop, [P]: Power
+[ESC]: Stop, [s] Start Connection With Spot
 
-t : up (+z)
-b : down (-z)
+[f]: Stand, [r]: Self-right
+[v]: Sit
 
-anything else : stop
-
-q/z : increase/decrease max speeds by 10%
-w/x : increase/decrease only linear speed by 10%
-e/c : increase/decrease only angular speed by 10%
 
 CTRL-C to quit
 """
 
 moveBindings = {
-		'i':(1,0,0,0),
-		'o':(1,0,0,-1),
-		'j':(0,0,0,1),
-		'l':(0,0,0,-1),
-		'u':(1,0,0,1),
-		',':(-1,0,0,0),
-		'.':(-1,0,0,1),
-		'm':(-1,0,0,-1),
-		'O':(1,-1,0,0),
-		'I':(1,0,0,0),
-		'J':(0,1,0,0),
-		'L':(0,-1,0,0),
-		'U':(1,1,0,0),
-		'<':(-1,0,0,0),
-		'>':(-1,-1,0,0),
-		'M':(-1,1,0,0),
-		't':(0,0,1,0),
-		'b':(0,0,-1,0),
+		' ': 1, # toggle estop
+		'\t': 2, # quit program
+		'p': 3, # toggle power
+		'l': 4, # toggle lease
+		'r': 5, # self right
+		'v': 6, # sit
+		'f': 7, # stand
+		's': 8, # start
+		'O': 9,
+		'I': 10,
 	       }
 
 class KeyPublisher(Node):
@@ -68,11 +48,16 @@ class KeyPublisher(Node):
 
     def publish_key(self):
         msg = String()
-        msg.data = 'a'  # Simulating 'a' key press for stop command
+
+
+        # CONNECT THIS TO getKey() 
+        # msg.data = 'a'  # Simulating 'a' key press for stop command
+                             
         self.publisher_.publish(msg)
         self.get_logger().info(f'Published: {msg.data}')
 
     def getKey():
+        #enter is not required, takes the raw inputs from the keyboards
         tty.setraw(sys.stdin.fileno())
         select.select([sys.stdin], [], [], 0)
         key = sys.stdin.read(1)
@@ -85,6 +70,8 @@ def main(args=None):
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
+
+
 
 if __name__ == '__main__':
     main()
--- subscriber_SpotControl.py
+++ subscriber_SpotControl.py
@@ -15,7 +15,7 @@ from PIL import Image, ImageEnhance
 
 import bosdyn.api.basic_command_pb2 as basic_command_pb2
 import bosdyn.api.power_pb2 as PowerServiceProto
-# import bosdyn.api.robot_command_pb2 as robot_command_pb2
+import bosdyn.api.robot_command_pb2 as robot_command_pb2
 import bosdyn.api.robot_state_pb2 as robot_state_proto
 import bosdyn.api.spot.robot_command_pb2 as spot_command_pb2
 import bosdyn.client.util
@@ -40,39 +40,50 @@ VELOCITY_BASE_ANGULAR = 0.8  # rad/sec
 VELOCITY_CMD_DURATION = 0.6  # seconds
 COMMAND_INPUT_RATE = 0.1
 
+
+from dotenv import load_dotenv
+# Load the .env file
+load_dotenv()
+# Retrieve the values from the environment variables
+ROBOT_IP = os.getenv('ROBOT_IP')
+USERNAME = os.getenv('USERNAME')
+PASSWORD = os.getenv('PASSWORD')
+
+
+
 def _grpc_or_log(desc, thunk):
     try:
         return thunk()
     except (ResponseError, RpcError) as err:
         LOGGER.error('Failed %s: %s', desc, err)
+AsyncRobotStateAsyncPeriodicQuery
 
+# for exiting loops ('while' loops, etc)
+# class ExitCheck(object):
+    # """A class to help exiting a loop, also capturing SIGTERM to exit the loop."""
 
+    # def __init__(self):
+    #     self._kill_now = False
+    #     signal.signal(signal.SIGTERM, self._sigterm_handler)
+    #     signal.signal(signal.SIGINT, self._sigterm_handler)
 
-class ExitCheck(object):
-    """A class to help exiting a loop, also capturing SIGTERM to exit the loop."""
+    # def __enter__(self):
+    #     return self
 
-    def __init__(self):
-        self._kill_now = False
-        signal.signal(signal.SIGTERM, self._sigterm_handler)
-        signal.signal(signal.SIGINT, self._sigterm_handler)
+    # def __exit__(self, _type, _value, _traceback):
+    #     return False
 
-    def __enter__(self):
-        return self
+    # def _sigterm_handler(self, _signum, _frame):
+    #     self._kill_now = True
 
-    def __exit__(self, _type, _value, _traceback):
-        return False
+    # def request_exit(self):
+    #     """Manually trigger an exit (rather than sigterm/sigint)."""
+    #     self._kill_now = True
 
-    def _sigterm_handler(self, _signum, _frame):
-        self._kill_now = True
-
-    def request_exit(self):
-        """Manually trigger an exit (rather than sigterm/sigint)."""
-        self._kill_now = True
-
-    @property
-    def kill_now(self):
-        """Return the status of the exit checker indicating if it should exit."""
-        return self._kill_now
+    # @property
+    # def kill_now(self):
+    #     """Return the status of the exit checker indicating if it should exit."""
+    #     return self._kill_now
 
 
 class CursesHandler(logging.Handler):
@@ -89,7 +100,20 @@ class CursesHandler(logging.Handler):
 
 
 class AsyncRobotState(AsyncPeriodicQuery):
+
+    #  What is the passed parameter it is handling? :
+        """ query name, SDK client, timing of queries, handled by the state client passed to it """
+    
     """Grab robot state."""
+
+        """Includes a few key states:
+        1. Battery State: Charge level, status, health
+        2. Motor State: Temperatures, power consumption, errors/warnings
+        3. Joint State: Positions, Velocitoes, efforts of the joints
+        4. Kinematic State: Robot's current pose and position in the world 
+        5. Sensor Data: Data from the robot's various sensors, such as cameras, LIDAR, and force sensors
+        6. E-Stop State
+        7. Power State: active power faults or warnings"""
 
     def __init__(self, robot_state_client):
         super(AsyncRobotState, self).__init__('robot_state', robot_state_client, LOGGER,
@@ -98,29 +122,54 @@ class AsyncRobotState(AsyncPeriodicQuery):
     def _start_query(self):
         return self._client.get_robot_state_async()
 
-class SpotControl(object):
-    """A curses interface for driving the robot."""
+
+class SpotControlInterface(object):
 
     def __init__(self, robot):
-        self._robot = robot
-        # Create clients -- do not use the for communication yet.
-        self._lease_client = robot.ensure_client(LeaseClient.default_service_name)
+
+        sdk = bosdyn.client.create_standard_sdk('CircularPathingClient')
+
+        #gets the IP address from the robot from the 
+        robot = sdk.create_robot(ROBOT_IP)
+        bosdyn.client.util.authenticate(robot, USERNAME, PASSWORD)
+
+        # Time-sync
+        time_sync_client = robot.ensure_client(TimeSyncClient.default_service_name)
+        bosdyn.client.util.sync_robot_time(robot)
+
+        assert not robot.is_estopped(),'Robot is estopped. Please use an external E-Stop client to configure E-Stop.'
+
+
+        # Create the lease client and grab the lease
+        lease_client = robot.ensure_client(bosdyn.client.lease.LeaseClient.default_service_name)
+        # Command client
+        command_client = robot.ensure_client(RobotCommandClient.default_service_name)
+        # E-Stop client
         try:
             self._estop_client = self._robot.ensure_client(EstopClient.default_service_name)
+            '''Need to know whats happening below, come back '''
             self._estop_endpoint = EstopEndpoint(self._estop_client, 'GNClient', 9.0)
         except:
             # Not the estop.
             self._estop_client = None
             self._estop_endpoint = None
-        self._power_client = robot.ensure_client(PowerClient.default_service_name)
-        self._robot_state_client = robot.ensure_client(RobotStateClient.default_service_name)
-        self._robot_command_client = robot.ensure_client(RobotCommandClient.default_service_name)
+        # Robot state client
+        robot_state_client = robot.ensure_client(RobotStateClient.default_service_name)
+        # Power client
+        power_client = robot.ensure_client(PowerClient.default_service_name)
+        # Data acquisition client
+        data_acquisition_client = robot.ensure_client(DataAcquisitionClient.default_service_name)
+
+        #initiate power on
+        self._power_motors()
         self._robot_state_task = AsyncRobotState(self._robot_state_client)
+        self._lock = threading.Lock()
         
         self._async_tasks = AsyncTasks([self._robot_state_task])
         self._lock = threading.Lock()
 
         self._locked_messages = ['', '', '']  # string: displayed message for user
+       
         self._estop_keepalive = None
         self._exit_check = None
 
@@ -128,6 +177,9 @@ class SpotControl(object):
         self._robot_id = None
         self._lease_keepalive = None
 
+######################################## Initializing/Shutting Down Methods #############################################
+    
+    
     def start(self):
         """Begin communication with the robot."""
         # Construct our lease keep-alive object, which begins RetainLease calls in a thread.
@@ -139,38 +191,112 @@ class SpotControl(object):
             self._estop_endpoint.force_simple_setup(
             )  # Set this endpoint as the robot's sole estop.
 
+    ''' will need this after the program is requested to be exited '''
     def shutdown(self):
         """Release control of robot as gracefully as possible."""
-        LOGGER.info('Shutting down WasdInterface.')
+        self._sit()
         if self._estop_keepalive:
             # This stops the check-in thread but does not stop the robot.
             self._estop_keepalive.shutdown()
         if self._lease_keepalive:
             self._lease_keepalive.shutdown()
+    
+        #DIRECTLY CALLED (FOR EXITING A WHILE LOOP)
+    # def _quit_program(self):
+        #     ''' Quits Program Via Exit Check Method '''
+        #     self._sit()
+        #     if self._exit_check is not None:
+        #         self._exit_check.request_exit()
 
-    def flush_and_estop_buffer(self, stdscr):
-        """Manually flush the curses input buffer but trigger any estop requests (space)"""
-        key = ''
-        while key != -1:
-            key = stdscr.getch()
-            if key == ord(' '):
-                self._toggle_estop()
+    def _request_power_on(self):
+        '''  PLACE HOLDER FOR EXPLANATION '''
+        request = PowerServiceProto.PowerCommandRequest.REQUEST_ON
+        return self._power_client.power_command_async(request)
+        
+    def _power_motors(self):
+        """Powers the motors on in the robot. """
 
-    def add_message(self, msg_text):
-        """Display the given message string to the user in the curses interface."""
-        with self._lock:
-            self._locked_messages = [msg_text] + self._locked_messages[:-1]
+        if self.motors_powered or not self.has_robot_control or not self.estop_keepalive or self.robot.is_powered_on(
+        ):
+            return
 
-    def message(self, idx):
-        """Grab one of the 3 last messages added."""
-        with self._lock:
-            return self._locked_messages[idx]
+        self.robot.power_on(timeout_sec=20)
+        assert robot.is_powered_on(), 'Robot power on failed.'
+        self.motors_powered = True
+
+    #DIRECTLY CALLED
+    def _toggle_estop(self):
+        """toggle estop on/off. Initial state is ON"""
+        if self._estop_client is not None and self._estop_endpoint is not None:
+            if not self._estop_keepalive:
+                self._estop_keepalive = EstopKeepAlive(self._estop_endpoint)
+            else:
+                self._try_grpc('stopping estop', self._estop_keepalive.stop)
+                self._estop_keepalive.shutdown()
+                self._estop_keepalive = None
+
+    #DIRECTLY CALLED
+    def _toggle_power(self):
+        '''  PLACE HOLDER FOR EXPLANATION '''
+        power_state = self._power_state()
+        if power_state is None:
+            self.add_message('Could not toggle power because power state is unknown')
+            return
+
+        if power_state == robot_state_proto.PowerState.STATE_OFF:
+            self._try_grpc_async('powering-on', self._request_power_on)
+
+        else:
+            self._try_grpc('powering-off', self._safe_power_off)
+
+    #DIRECTLY CALLED
+    def _toggle_lease(self):
+        """toggle lease acquisition. Initial state is acquired"""
+        if self._lease_client is not None:
+            if self._lease_keepalive is None:
+                self._lease_keepalive = LeaseKeepAlive(self._lease_client, must_acquire=True,
+                                                       return_at_exit=True)
+            else:
+                self._lease_keepalive.shutdown()
+                self._lease_keepalive = None
 
     @property
     def robot_state(self):
         """Get latest robot state proto."""
         return self._robot_state_task.proto
 
+    def _try_grpc(self, desc, thunk):
+        '''  PLACE HOLDER FOR EXPLANATION '''
+        try:
+            return thunk()
+        except (ResponseError, RpcError, LeaseBaseError) as err:
+            # self.add_message(f'Failed {desc}: {err}')
+            return None
+
+    
+    def _try_grpc_async(self, desc, thunk):
+        '''  PLACE HOLDER FOR EXPLANATION '''
+        def on_future_done(fut):
+            try:
+                fut.result()
+            except (ResponseError, RpcError, LeaseBaseError) as err:
+                # self.add_message(f'Failed {desc}: {err}')
+                return None
+
+        future = thunk()
+        future.add_done_callback(on_future_done)
+
+    # start allowing robot commands to happen
+    def _start_robot_command(self, desc, command_proto, end_time_secs=None):
+
+        def _start_command():
+            self._robot_command_client.robot_command(command=command_proto,
+                                                        end_time_secs=end_time_secs)
+
+        self._try_grpc(desc, _start_command) 
+    
+
+######################################## Other Methods (Movement Related Etc.) #############################################
 
 
 
